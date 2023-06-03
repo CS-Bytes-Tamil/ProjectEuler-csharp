49ff3dc858df1b9924dae704fdbf90a30aa30c42
nemo <csbytestamil@gmail.com>
2023-06-03 09:32:41 +0530

    Multiples of 3 and 5

---
+++ #1-Multiple of 3 and 5/Multipleof3and5/Multipleof3and5/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+GetMultiplesof3and5(Convert.ToInt32(Console.ReadLine()));
+
+void GetMultiplesof3and5(int range)
+{
+    double sum = 0;
+    for (int i = 0; i < range; i++)
+    {
+        if (i % 3 == 0 || i % 5 == 0)
+        {
+            sum += i;
+        }
+    }
+
+    Console.WriteLine(sum);
+}
+
